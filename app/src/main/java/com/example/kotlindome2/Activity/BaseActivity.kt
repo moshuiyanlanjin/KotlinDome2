9f3427e36e91dff22789ada1c72ebc35b57ebc3b
moshuiyanlanjin <1078314344@qq.com>
2020-09-15 17:28:12 +0800

    完成退出

--- app/src/main/java/com/example/kotlindome2/Activity/BaseActivity.kt
+++ app/src/main/java/com/example/kotlindome2/Activity/BaseActivity.kt
@@ -1,16 +1,50 @@
 package com.example.kotlindome2.Activity
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.example.kotlindome2.MainActivity
 import com.example.kotlindome2.Utils.ActivityCollector
 import com.example.kotlindome2.Utils.LogUtils
 
 open class BaseActivity : AppCompatActivity(){
+    lateinit var receive : ForceOfflineReceive
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         LogUtils.d(this,"当前的Activity是${javaClass.simpleName}")
         ActivityCollector.AddActivity(this)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val intentFilter = IntentFilter()
+        intentFilter.addAction("com.example.CORRE")
+        receive = ForceOfflineReceive()
+        registerReceiver(receive,intentFilter)
+    }
+
+    inner class ForceOfflineReceive : BroadcastReceiver() {
+        override fun onReceive(p0: Context?, p1: Intent?) {
+
+            p0?.let {
+                AlertDialog.Builder(it).apply {
+                    setTitle("退出登录")
+                    setMessage("是否退出登录")
+                    setCancelable(false)
+                    setPositiveButton("确定"){_,_->
+                        ActivityCollector.FinshAllActivity()
+                        val i = Intent(p0,MainActivity::class.java)
+                        p0.startActivity(i)
+                    }
+                }
+            }
+        }
+
     }
 
     override fun onDestroy() {
